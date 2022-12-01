392e9613370afa951cc04d71527c48b7df954d3c
bemmanue <myaushenka@gmail.com>
2022-12-01 23:55:00 +0300

    ex01 is in process

--- ex01/createBlog.go
+++ ex01/createBlog.go
@@ -7,12 +7,20 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 )
+
+type Message struct {
+	Id      int
+	Message string
+}
 
 func main() {
 	http.HandleFunc("/", showBlog)
 	http.HandleFunc("/admin", postArticles)
 	http.HandleFunc("/publication", publishArticle)
+	http.HandleFunc("/?page=", getPage)
 
 	styleHandler := http.FileServer(http.Dir("./css"))
 	http.Handle("/css/", http.StripPrefix("/css", styleHandler))
@@ -23,9 +31,48 @@ func main() {
 	}
 }
 
+func getPage(w http.ResponseWriter, r *http.Request) {
+	_, err := strconv.Atoi(strings.TrimPrefix(r.URL.RawQuery, "page="))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+}
+
 func showBlog(w http.ResponseWriter, r *http.Request) {
+	connStr := "user=uliakulikova dbname=uliakulikova sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT * FROM blog ORDER BY id LIMIT 2 OFFSET 2")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	var messages []Message
+
+	for rows.Next() {
+		p := Message{}
+		err := rows.Scan(&p.Id, &p.Message)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		messages = append(messages, p)
+	}
+
+	data := struct {
+		Msg []Message
+	}{Msg: messages}
+
 	tmpl, _ := template.ParseFiles("templates/index.html")
-	err := tmpl.Execute(w, nil)
+	err = tmpl.Execute(w, data)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -38,7 +85,7 @@ func postArticles(w http.ResponseWriter, r *http.Request) {
 func publishArticle(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, r.FormValue("message"))
 
-	connStr := "user=bemmanue dbname=bemmanue sslmode=disable"
+	connStr := "user=uliakulikova dbname=uliakulikova sslmode=disable"
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatalln(err)
